5dcb6625f8a31f45293eb1c1d87e68c76d4059e4
True Ghiassi <true@ghiassitrio.co.uk>
2022-11-17 20:41:25 +0000

    Trailing list bracket is now formatted correctly

--- main.c
+++ main.c
@@ -430,6 +430,26 @@ static void format_list_level(
 		} else {
 			++column;
 		}
+		if (one[one_i+1] == ']' && has_newlines) {
+			int init_two_i = two_i;
+			--two_i;
+			for (; two[two_i] == ' '; --two_i) {
+			}
+
+			if (two[two_i] == '\n') {
+				two_i = init_two_i;
+			} else {
+				++two_i;
+				two[two_i] = '\n';
+				++two_i;
+
+				for (int i = 0; i < start_column; ++i) {
+					two[two_i] = ' ';
+					++two_i;
+				}
+				++one_i;
+			}
+		}
 		if (nesting == nesting_level && one[one_i-1] == '[') {
 			two[two_i] = ' ';
 			++two_i;
@@ -539,16 +559,17 @@ static void format_expression(
 
 	int i = 0;
 	for (; max_nesting > 0 && !equal_buffers(one, two, *one_size, *two_size); ++i) {
+		printf("here: %d\n", i);
 
 		if (i%2 == 0) {
-			format_list_pass(one, two, one_size, two_size, max_nesting);
+			format_list_pass(two, one, two_size, one_size, max_nesting);
 			continue;
 		}
 
-		format_list_pass(two, one, two_size, one_size, max_nesting);
+		format_list_pass(one, two, one_size, two_size, max_nesting);
 	}
 
-	if (i%2 == 1) {
+	if (i%2 == 0) {
 		return;
 	}
 
