32241106e0d790880724c17fd8645f7569029bec
True Ghiassi <true@ghiassitrio.co.uk>
2022-11-24 19:55:00 +0000

    Corruption still there

--- src/Tests.elm
+++ src/Tests.elm
@@ -1,21 +1,3 @@
-import Css.Animations exposing (Keyframes)
-import Css.Internal exposing (getOverloadedProperty, lengthConverter, lengthForOverloadedProperty)
-import Css.Preprocess as Preprocess exposing (Style, unwrapSnippet)
-import Css.String
-import Css.Structure as Structure exposing (..)
-import Hex
-import String
-
-
-{-| -}
-type alias Style =
-    Preprocess.Style
-
-
-
-{- Length -}
-
-
 cssFunction : String -> List String -> String
 cssFunction funcName args =
     funcName
