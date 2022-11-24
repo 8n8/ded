ca9e51d8d6ef8a6d4119f2ef41a8c7ea6ca5221f
True Ghiassi <true@ghiassitrio.co.uk>
2022-11-24 20:03:28 +0000

    Corruption still there

--- src/Tests.elm
+++ src/Tests.elm
@@ -1,20 +1,2 @@
 calc firstExpr expression secondExpr =
-    let
-        value =
             cssFunction "calc" [ calcs ]
-    in
-    { value = value
-    , length = Compatible
-    , lengthOrAuto = Compatible
-    , lengthOrNumber = Compatible
-    , lengthOrNone = Compatible
-    , lengthOrMinMaxDimension = Compatible
-    , lengthOrNoneOrMinMaxDimension = Compatible
-    , textIndent = Compatible
-    , flexBasis = Compatible
-    , lengthOrNumberOrAutoOrNoneOrContent = Compatible
-    , fontSize = Compatible
-    , lengthOrAutoOrCoverOrContain = Compatible
-    , lineHeight = Compatible
-    , calc = Compatible
-    }
