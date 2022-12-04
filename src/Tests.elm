7eb0d82638f42a3fb848363b01a5c7d918ac11a8
True Ghiassi <true@ghiassitrio.co.uk>
2022-12-04 18:30:25 +0000

    Bug is still there

--- src/Tests.elm
+++ src/Tests.elm
@@ -405,17 +405,4 @@ calc firstExpr expression secondExpr =
             cssFunction "calc" [ calcs ]
     in
     { value = value
-    , length = Compatible
-    , lengthOrAuto = Compatible
-    , lengthOrNumber = Compatible
-    , lengthOrNone = Compatible
-    , lengthOrMinMaxDimension = Compatible
-    , lengthOrNoneOrMinMaxDimension = Compatible
-    , textIndent = Compatible
-    , flexBasis = Compatible
-    , lengthOrNumberOrAutoOrNoneOrContent = Compatible
-    , fontSize = Compatible
-    , lengthOrAutoOrCoverOrContain = Compatible
-    , lineHeight = Compatible
-    , calc = Compatible
     }
