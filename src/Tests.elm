d6839a9a6015f3a5286483d285fa42d74093f672
True Ghiassi <true@ghiassitrio.co.uk>
2022-12-05 21:44:43 +0000

    Smaller test file and bug still there

--- src/Tests.elm
+++ src/Tests.elm
@@ -4,12 +4,10 @@ module ParserTest exposing (expr)
 expr : Test
 expr =
     describe "Stage.Parse.Parser.expr"
-        (List.map runSection
-            [ ( "literal multiline string"
-              , [ ( "escape backslash"
-                  , tripleQuote "\\\\"
-                  )
-                ]
+        [ ( "literal multiline string"
+          , [ ( "escape backslash"
+              , tripleQuote "\\\\"
               )
             ]
-        )
+          )
+        ]
