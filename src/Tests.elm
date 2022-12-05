a480c5f9490ef83ac5d10e3d5735851adcc7d2f4
True Ghiassi <true@ghiassitrio.co.uk>
2022-12-05 21:40:49 +0000

    Smaller test file and bug still there

--- src/Tests.elm
+++ src/Tests.elm
@@ -18,62 +18,7 @@ expr =
     in
     describe "Stage.Parse.Parser.expr"
         (List.map runSection
-            [ ( "literal string"
-              , [ ( "empty"
-                  , doubleQuote ""
-                  , Just (String "")
-                  )
-                , ( "one space"
-                  , doubleQuote " "
-                  , Just (String " ")
-                  )
-                , ( "newline shouldn't work"
-                  , doubleQuote "\n"
-                  , Nothing
-                  )
-                , ( "two numbers"
-                  , doubleQuote "42"
-                  , Just (String "42")
-                  )
-                , ( "single quote"
-                  , doubleQuote "'"
-                  , Just (String "'")
-                  )
-                , ( "double quote"
-                  , doubleQuote "\\\""
-                  , Just (String "\"")
-                  )
-                , ( "escape backslash"
-                  , doubleQuote "\\\\"
-                  , Just (String "\\")
-                  )
-                , ( "escape n"
-                  , doubleQuote "\\n"
-                  , Just (String "\n")
-                  )
-                , ( "escape r"
-                  , doubleQuote "\\r"
-                  , Just (String "\u{000D}")
-                  )
-                , ( "escape t"
-                  , doubleQuote "\\t"
-                  , Just (String "\t")
-                  )
-                , ( "emoji"
-                  , doubleQuote "😃"
-                  , Just (String "😃")
-                  )
-                , ( "escaped unicode code point"
-                  , doubleQuote "\\u{1F648}"
-                  , Just (String "🙈")
-                  )
-                , ( "combo of escapes and chars"
-                  , doubleQuote "\\u{1F648}\\n\\r\\t\\\\abc123"
-                  , Just (String "🙈\n\u{000D}\t\\abc123")
-                  )
-                ]
-              )
-            , ( "literal multiline string"
+            [ ( "literal multiline string"
               , [ ( "empty"
                   , tripleQuote ""
                   , Just (String "")
