82679b97f0e6b09ade1a277911faf24d82909510
True Ghiassi <true@ghiassitrio.co.uk>
2022-12-05 21:44:26 +0000

    Smaller test file and bug still there

--- src/Tests.elm
+++ src/Tests.elm
@@ -3,29 +3,11 @@ module ParserTest exposing (expr)
 
 expr : Test
 expr =
-    let
-        runSection ( description, tests ) =
-            describe description
-                (List.map runTest tests)
-
-        runTest ( description, input, output ) =
-            test description <|
-                \() ->
-                    input
-                        |> P.run Stage.Parse.Parser.expr
-                        |> Result.map Frontend.unwrap
-                        |> expectEqualParseResult input output
-    in
     describe "Stage.Parse.Parser.expr"
         (List.map runSection
             [ ( "literal multiline string"
-              , [ ( "double quote"
-                  , tripleQuote " \" "
-                  , Just (String " \" ")
-                  )
-                , ( "escape backslash"
+              , [ ( "escape backslash"
                   , tripleQuote "\\\\"
-                  , Just (String "\\")
                   )
                 ]
               )
