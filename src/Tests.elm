2db783a6ab91834298eec8557dda5ad1783569e7
True Ghiassi <true@ghiassitrio.co.uk>
2022-12-28 22:12:07 +0000

    Smaller test file

--- src/Tests.elm
+++ src/Tests.elm
@@ -2,17 +2,3 @@ type StringType
     = {- ' -} CharString
     | {- " -} NormalString
     | {- """ -} MultilineString
-
-
-canContinueChompingString : StringType -> Char -> Bool
-canContinueChompingString stringType char =
-    case stringType of
-        CharString ->
-            char /= '\''
-
-        NormalString ->
-            char /= '"' && char /= '\\'
-
-        MultilineString ->
-            -- we'll have to check for the other two double-quotes afterwards
-            char /= '"' && char /= '\\'
