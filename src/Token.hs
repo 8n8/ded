d97638b7096f02d940a725a426ba5597d5a2f864
True Ghiassi <true@ghiassitrio.co.uk>
2023-01-06 20:13:17 +0000

    Revert "A messy attempt at list formatting"
    This reverts commit b9d71801375de2389654696fa5b26594f152e3b2.

--- src/Token.hs
+++ src/Token.hs
@@ -12,8 +12,6 @@ data Token
   | Verbatim Verbatim.Verbatim
   | Start
   | End
-  | OpenBracket
-  | CloseBracket
   deriving (Show)
 
 asciiNewline :: Data.Word.Word8
@@ -30,30 +28,8 @@ parse =
     [ parseNewline,
       parseSpace,
       parseEquals,
-      parseOpenBracket,
-      parseCloseBracket,
       fmap Verbatim Verbatim.parse
     ]
-
-parseOpenBracket :: Data.Attoparsec.ByteString.Parser Token
-parseOpenBracket =
-    do
-    _ <- Data.Attoparsec.ByteString.word8 asciiOpenBracket
-    return OpenBracket
-
-parseCloseBracket :: Data.Attoparsec.ByteString.Parser Token
-parseCloseBracket =
-    do
-    _ <- Data.Attoparsec.ByteString.word8 asciiCloseBracket
-    return CloseBracket
-
-asciiOpenBracket :: Data.Word.Word8
-asciiOpenBracket =
-    91
-
-asciiCloseBracket :: Data.Word.Word8
-asciiCloseBracket =
-    93
 
 parseNewline :: Data.Attoparsec.ByteString.Parser Token
 parseNewline =
@@ -92,9 +68,3 @@ encode token =
       ""
     Equals ->
       "="
-
-    OpenBracket ->
-        "["
-
-    CloseBracket ->
-        "]"
